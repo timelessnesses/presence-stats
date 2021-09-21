3cc23cc9a14d7b2cdb1d76a9e6c4b779838f4086
Rukchad Wongprayoon <contact@biomooping.tk>
2021-09-21 18:54:36 +0700

    executable should be good

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ classifiers = []
  
 setup(
   name='pypresence-stats',
-  version='1.1',
+  version='1.2',
   description='Presence-stats but you can make python script and launch it.',
   long_description=open("README.md").read(),
   long_description_content_type='text/markdown',
