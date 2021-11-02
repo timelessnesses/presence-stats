8ea59c09c50c1323e2f72883627790dbaaa2c1dd
Rukchad Wongprayoon <contact@biomooping.tk>
2021-11-02 11:40:52 +0700

    Update presencestats.py

--- presencestats.py
+++ presencestats.py
@@ -32,31 +32,33 @@ def launch(client_id:str,pipe:int):
         presence.update(details=f"System Info of {socket.gethostname()}",state=detail,start=times,buttons=buttons)
         time.sleep(1)
         print("Updated Presence")
-if __name__ == '__main__':
-    def launcharg(client_id:str = sys.argv[1],pipe:int = sys.argv[2]):
-        presence = pypresence.Presence(client_id=client_id,pipe=pipe)
 
-        times = timer()
+def launcharg(client_id:str = 904942886070673479,pipe:int = 0):
+    try:
+        client_id = sys.argv[1]
+    except Exception:
+        pass
+    try:
+        pipe = sys.argv[2]
+    except Exception:
+        pass
+    presence = pypresence.Presence(client_id=client_id,pipe=pipe)
 
-        while True:
-            try:
-                presence.connect()
-            except:
-                print("Could not connect to Discord, retrying in 5 seconds")
-                time.sleep(5)
-            else:
-                break
-        while True:
-            presence.clear()
-            detail = f"""
-            CPU: {int(psutil.cpu_percent())}%
-            RAM: {int(psutil.virtual_memory().percent)}%
-            Disk: {int(psutil.disk_usage('/').percent)}%
-            Boot time: {timedelta(seconds=int(time.time() - times))}
-            Script Started for: {datetime.now() - datetime.fromtimestamp(times)}
+    times = timer()
+
+    while True:
+        try:
+            presence.connect()
+        except:
+            print("Could not connect to Discord, retrying in 5 seconds")
+            time.sleep(5)
+        else:
+            break
+    while True:
+        detail = f"""CPU: {int(psutil.cpu_percent())}%\nRAM: {int(psutil.virtual_memory().percent)}%\nDisk: {int(psutil.disk_usage('/').percent)}%\n\nStarted time: {timedelta(seconds=int(time.time() - times))}
             """
-            buttons = [{"label":"Want this? Click to Download!","url":"https://github.com/dumb-stuff/presence-stats"}]
-            presence.update(details=f"System Info of {socket.gethostname()}",state=detail,start=times,buttons=buttons)
-            time.sleep(1)
-            print("Updated Presence")
-    launcharg()
+        print(len(detail))
+        buttons = [{"label":"Want this? Click to Download!","url":"https://github.com/dumb-stuff/presence-stats"}]
+        presence.update(details=f"System Info of {socket.gethostname()}",state=detail,start=times,buttons=buttons)
+        time.sleep(1)
+        print("Updated Presence")
